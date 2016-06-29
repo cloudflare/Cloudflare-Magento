b6481ac22c27343ee9f2d78ca2f37aaa34f53c21
John Wineman <jwineman@cloudflare.com>
2016-06-29 10:59:41 -0700

    Client API GET /zones only returns the current Magento domain.

--- Backend/ClientRoutes.php
+++ Backend/ClientRoutes.php
@@ -4,5 +4,14 @@ namespace CloudFlare\Plugin\Backend;
 
 class ClientRoutes
 {
-    public static $routes = array();
+    public static $routes = array(
+        'zones' => array(
+            'class' => 'CloudFlare\Plugin\Backend\ClientActions',
+            'methods' => array(
+                'GET' => array(
+                    'function' => 'getZonesReturnMagentoZone'
+                )
+            )
+        )
+    );
 }
