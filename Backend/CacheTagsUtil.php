7135c02b7b8885f230e855411c7876368da71ad3
John Wineman <jwineman@cloudflare.com>
2016-07-21 13:26:02 +0100

    PI-695: X-Cache-Tags http header set with all Magento cache tags for the page being loaded.

---
+++ Backend/CacheTagsUtil.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace CloudFlare\Plugin\Backend;
+
+use \Psr\Log\LoggerInterface;
+
+class CacheTagsUtil
+{
+    const CLOUDFLARE_CACHE_TAG_HEADER = "X-Cache-Tags";
+
+    /**
+     * @var \Psr\Log\LoggerInterface
+     */
+    protected $logger;
+
+    public function __construct(LoggerInterface $logger) {
+        $this->logger = $logger;
+    }
+
+    /**
+     * Sets the X-Cache-Tags header(s) on the response
+     *
+     * @param $response
+     * @param $tags
+     * @return $response
+     */
+    public function setCloudFlareCacheTagsResponseHeader($response, $tags) {
+        $cacheTagHeaderList = $this->get255ByteXCacheTagHeaderStringValues($tags);
+
+        /*
+         * CloudFlare Cache Tags allow for multiple X-Cache-Tags headers but Magento2's
+         * $response->setHeader() doesn't allow for multiple http headers with the same name
+         * so we only set the first one in the list
+         */
+        if(count($cacheTagHeaderList) > 0) {
+            $response->setHeader(self::CLOUDFLARE_CACHE_TAG_HEADER, $cacheTagHeaderList[0]);
+            $this->logger->debug("CloudFlare header '". self::CLOUDFLARE_CACHE_TAG_HEADER . "' set with value '". $cacheTagHeaderList[0] ."'");
+
+            if(count($cacheTagHeaderList) > 1) {
+                $this->logger->debug("Some CloudFlare cache tags were not set because the total length of the list exceeded 255 bytes.");
+            }
+        }
+
+        return $response;
+    }
+
+    /**
+     * Generate list of X-Cache-Tag header values which are less than 255 bytes each
+     *
+     * @param $cacheTagList
+     * @return array
+     */
+    public function get255ByteXCacheTagHeaderStringValues($cacheTagList) {
+        $cacheTagHeaderList = array();
+        $cacheTagHeader = "";
+
+        foreach($cacheTagList as $cacheTag) {
+            $cacheTagHeaderEncoding = mb_detect_encoding($cacheTagHeader);
+            $cacheTagEncoding = mb_detect_encoding($cacheTag);
+
+            //Is this cache tag larger than 255 bytes?
+            if(mb_strlen($cacheTag, $cacheTagEncoding) > 255) {
+                array_push($cacheTagHeaderList, mb_strcut($cacheTag, 1, 255));
+            }
+            //Would appending the current cache tag to the cache tag header put it over the 255 byte limit?
+            else if((mb_strlen($cacheTagHeader, $cacheTagHeaderEncoding) + mb_strlen(",".$cacheTag, $cacheTagEncoding)) > 255) {
+                //Start new header
+                array_push($cacheTagHeaderList, $cacheTagHeader);
+                $cacheTagHeader = $cacheTag;
+            } else {
+                //Append cache tag to cache tag header
+                if($cacheTagHeader !== "") { //avoid creating headers that start with a comma.
+                    $cacheTagHeader = $cacheTagHeader . ",";
+                }
+                $cacheTagHeader = $cacheTagHeader . $cacheTag;
+            }
+        }
+
+        array_push($cacheTagHeaderList, $cacheTagHeader);
+
+        return $cacheTagHeaderList;
+    }
+}
---
+++ Model/Layout/LayoutPlugin.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace CloudFlare\Plugin\Model\Layout;
+
+use \Magento\Framework\App\ResponseInterface;
+use \Magento\PageCache\Model\Config;
+use \Psr\Log\LoggerInterface;
+use \CloudFlare\Plugin\Backend\CacheTagsUtil;
+class LayoutPlugin
+{
+    /**
+     * @var \Magento\PageCache\Model\Config
+     */
+    protected $config;
+
+    /**
+     * @var \Psr\Log\LoggerInterface
+     */
+    protected $logger;
+
+    /**
+     * @var \Magento\Framework\App\ResponseInterface
+     */
+    protected $response;
+
+    /**
+     * @var \CloudFlare\Plugin\Backend\CacheTagsUtil
+     */
+    protected $cacheTagsUtil;
+
+
+    /**
+     * Constructor
+     *
+     * @param ResponseInterface $response
+     * @param Config $config
+     * @param LoggerInterface $logger
+     * @param CacheTagsUtil $cacheTagsUtil
+     */
+    public function __construct(
+        ResponseInterface $response,
+        Config $config,
+        LoggerInterface $logger,
+        CacheTagsUtil $cacheTagsUtil
+    ) {
+        $this->response = $response;
+        $this->config = $config;
+        $this->logger = $logger;
+        $this->cacheTagsUtil = $cacheTagsUtil;
+
+    }
+
+
+    /**
+     * Set X-Cache-Tags header with all the Magento Cache Tags so
+     * they can be purged by the CloudFlare API
+     *
+     * @param \Magento\Framework\View\Layout $subject
+     * @param $result
+     * @return mixed
+     */
+    public function afterGetOutput(\Magento\Framework\View\Layout $subject, $result)
+    {
+        if ($subject->isCacheable() && $this->config->isEnabled()) {
+            $tags = [];
+            foreach ($subject->getAllBlocks() as $block) {
+                if ($block->getIdentities() !== null) {
+                    $tags = array_merge($tags, $block->getIdentities());
+                }
+            }
+            $tags = array_unique($tags);
+
+            $this->cacheTagsUtil->setCloudFlareCacheTagsResponseHeader($this->response, $tags);
+        }
+
+        return $result;
+    }
+}
